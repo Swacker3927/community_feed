eb9f05e7dbf84a5853ad0c190a5f8ba674bcd4d4
Taewoo Lee <sai6321@naver.com>
2025-03-08 21:48:29 +0900

    Initial Commit

---
+++ src/main/java/org/fastcampus/user/domain/User.java
@@ -0,0 +1,58 @@
+package org.fastcampus.user.domain;
+
+import java.util.Objects;
+import org.fastcampus.common.domain.*;
+
+public class User {
+    private final Long id;
+    private final UserInfo info;
+    private final PositiveIntegerCounter followingCount;
+    private final PositiveIntegerCounter followerCount;
+
+    public User(Long id, UserInfo userInfo) {
+        this.id = id;
+        this.info = userInfo;
+        this.followingCount = new PositiveIntegerCounter();
+        this.followerCount = new PositiveIntegerCounter();
+    }
+
+    public void follow(User targetUser) {
+        if (targetUser.equals(this)) {
+            throw new IllegalArgumentException();
+        }
+
+        followingCount.increase();
+        targetUser.increaseFollowerCount();
+    }
+
+    public void unfollow(User targetUser) {
+        if (targetUser.equals(this)) {
+            throw new IllegalArgumentException();
+        }
+
+        followingCount.decrease();
+        targetUser.decreaseFollowerCount();
+    }
+
+    private void increaseFollowerCount() {
+        followerCount.increase();
+    }
+
+    private void decreaseFollowerCount() {
+        followerCount.decrease();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        User user = (User) o;
+        return Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
