e2147db529a1958f8d9f0b59b63150688564ebf7
Taewoo Lee <sai6321@naver.com>
2025-03-24 22:41:47 +0900

    커뮤니티 피드 서비스에 Spring 적용하기

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     id("java")
+    id("org.springframework.boot") version "3.4.4"
+    id("io.spring.dependency-management") version "1.0.15.RELEASE"
 }
 
 group = "org.fastcampus"
@@ -10,8 +12,11 @@ repositories {
 }
 
 dependencies {
+    implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.boot:spring-boot-starter-web")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
 tasks.test {
